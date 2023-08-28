fff62d1602d1927b946167bada3c9365187af6f0
Matthew Wasylko <matthew.wasylko@gmail.com>
2023-08-28 16:01:22 +0100

    First impression, MVP completed in Console, App, Repositories, Controllers, Models, db. HTML and Static to be completed.

--- vet_app/app.py
+++ vet_app/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request
-# from controllers.vets_controllers import vets_blueprint
-# from controllers.pets_controllers import pets_blueprint
+from controllers.vet_controller import vets_blueprint
+from controllers.pet_controller import pets_blueprint
 # from controllers.owners_controllers import owners_blueprint
+# from controllers.notes_controllers import notes_blueprint
 
 app = Flask(__name__)
 
-#app.register_blueprint(vets_blueprint)
-#app.register_blueprint(pets_blueprint)
-#app.register_blueprint(owners_blueprint)
+app.register_blueprint(vets_blueprint)
+app.register_blueprint(pets_blueprint)
+# app.register_blueprint(owners_blueprint)
+# app.register_blueprint(notes_blueprint)
 
-#@app.route('/')
-#def home():
-#   return render_template('index.html')
+@app.route('/')
+def home():
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
